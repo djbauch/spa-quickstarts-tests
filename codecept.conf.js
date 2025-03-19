97da9b0ab7a92124b7bfe9aaf36089b30b145eb0
DJ Bauch <dj.bauch@gmail.com>
2025-03-19 17:17:56 -0500

    Try using bootstrapAll() to await the puppeteer installation.

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,56 +1,65 @@
 let url;
-if (process.env.WEBAPP_URL){
+if (process.env.WEBAPP_URL) {
   url = process.env.WEBAPP_URL;
-} else if (process.env.SAMPLE_PORT){
+} else if (process.env.SAMPLE_PORT) {
   url = "http://localhost:" + process.env.SAMPLE_PORT;
 } else {
   url = "http://localhost:3000";
 }
 
 const { exec } = require("child_process");
+const util = require("util");
 
-exec("npx puppeteer browsers install chrome", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log('Executed npx puppeteer browsers install chrome');
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-}
-);
+const execPromise = util.promisify(exec);
 
-exec("npm list", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log('npm list output');
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+// Function to install Puppeteer and then list npm packages
+async function installPuppeteer() {
+  execPromise("npx puppeteer browsers install chrome")
+    .then(({ stdout, stderr }) => {
+      console.log("Executed npx puppeteer browsers install chrome");
+      console.log(`stdout: ${stdout}`);
+      if (stderr) console.error(`stderr: ${stderr}`);
+
+      // Now run npm list after Puppeteer installation is complete
+      return execPromise("npm list");
+    })
+    .then(({ stdout, stderr }) => {
+      console.log("npm list output");
+      console.log(`stdout: ${stdout}`);
+      if (stderr) console.error(`stderr: ${stderr}`);
+    })
+    .catch((error) => {
+      console.error(`exec error: ${error}`);
+    });
 }
-);
 
 exports.config = {
-  "tests": "./*_test.js",
-  "timeout": 10000,
-  "output": "out",
-  "multiple": {
-    "basic": {
-      "browsers": ["chrome", "firefox"]
-    }
+  async bootstrapAll() {
+    console.log("Running bootstrapAll");
+    await installPuppeteer();
   },
-  "helpers": {
-    "Puppeteer": {
-      "url": url,
-      "chrome":{
-        "args": ["--no-sandbox", "--disable-setuid-sandbox"]
-      }
-    }
+  async teardownAll() {
+    console.log("Running teardownAll after all tests are done");
   },
-  "include": {},
-  "bootstrap": false,
-  "mocha": {},
-  "name": "webapp-tests",
-  "require": ["puppeteer"],
-}
+  tests: "./*_test.js",
+  timeout: 10000,
+  output: "out",
+  multiple: {
+    basic: {
+      browsers: ["chrome", "firefox"],
+    },
+  },
+  helpers: {
+    Puppeteer: {
+      url: url,
+      chrome: {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      },
+    },
+  },
+  include: {},
+  bootstrap: false,
+  mocha: {},
+  name: "webapp-tests",
+  require: ["puppeteer"],
+};
