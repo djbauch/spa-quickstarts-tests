a3f7bb4c0a48cea165ca7270f075149a2ae13e27
DJ Bauch <dj.bauch@gmail.com>
2025-03-19 23:19:02 -0500

    npx codeceptjs info didn't work

--- config_test.js
+++ config_test.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("npx codeceptjs info", (error, stdout, stderr) => {
+exec("env", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error executing command: ${error.message}`);
     return;
