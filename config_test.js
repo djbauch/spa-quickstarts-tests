dbfc2534ed2b024cadfd7d7c8fb17d09422cc1f5
DJ Bauch <dj.bauch@gmail.com>
2025-03-19 22:01:05 -0500

    Too many o's in codeceptjs

--- config_test.js
+++ config_test.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("npx coodeceptjs info", (error, stdout, stderr) => {
+exec("npx codeceptjs info", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error executing command: ${error.message}`);
     return;
