db7a3c32a88374c97fe1d86bb81ba8225ce475c7
DJ Bauch <dj.bauch@gmail.com>
2025-03-19 21:18:50 -0500

    Print out info from codeceptjs

--- config_test.js
+++ config_test.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("npm list", (error, stdout, stderr) => {
+exec("npx coodeceptjs info", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error executing command: ${error.message}`);
     return;
