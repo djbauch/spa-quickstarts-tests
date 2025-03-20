2ebab64319e7ac437be2af7d28c7b37b9ab4db0d
DJ Bauch <dj.bauch@gmail.com>
2025-03-20 00:45:18 -0500

    Fixed the lock_login_spa_test search for the form submit button

--- lock_login_spa_test.js
+++ lock_login_spa_test.js
@@ -8,7 +8,7 @@ Scenario("Log in using Lock", ({ I }) => {
   I.wait(1);
   I.fillField('input[name="username"]', "asdasd");
   I.fillField('input[name="password"]', "asdasd");
-  I.click(".auth0-lock-widget-container .auth0-lock-submit");
+  I.click("button[type='submit']");
   I.waitForVisible("#profileDropDown", 20);
   I.wait(3);
   I.click("#profileDropDown");
