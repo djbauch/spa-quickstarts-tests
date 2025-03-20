d28c5526b89b13f5af5eb49ba03184bc69dd96b3
DJ Bauch <dj.bauch@gmail.com>
2025-03-19 23:37:38 -0500

    Try switching from chrome (not installed) to Playwright

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,42 +7,7 @@ if (process.env.WEBAPP_URL) {
   url = "http://localhost:3000";
 }
 
-const { exec } = require("child_process");
-const util = require("util");
-
-const execPromise = util.promisify(exec);
-
-// Function to install Puppeteer and then list npm packages
-async function installPuppeteer() {
-  execPromise(
-    "npm install -g npm@11.2.0; npx @puppeteer/browsers install chrome@latest"
-  )
-    .then(({ stdout, stderr }) => {
-      console.log("Executed npx puppeteer browsers install chrome");
-      console.log(`stdout: ${stdout}`);
-      if (stderr) console.error(`stderr: ${stderr}`);
-
-      // Now run npm list after Puppeteer installation is complete
-      return execPromise("npm list");
-    })
-    .then(({ stdout, stderr }) => {
-      console.log("npm list output");
-      console.log(`stdout: ${stdout}`);
-      if (stderr) console.error(`stderr: ${stderr}`);
-    })
-    .catch((error) => {
-      console.error(`exec error: ${error}`);
-    });
-}
-
 exports.config = {
-  async bootstrapAll() {
-    console.log("Running bootstrapAll");
-    await installPuppeteer();
-  },
-  async teardownAll() {
-    console.log("Running teardownAll after all tests are done");
-  },
   tests: "./*_test.js",
   timeout: 10000,
   output: "out",
@@ -52,16 +17,15 @@ exports.config = {
     },
   },
   helpers: {
-    Puppeteer: {
+    Playwright: {
       url: url,
-      chrome: {
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      },
+      show: false,
+      browser: "chromium",
+      waitForNavigation: "networkidle0",
     },
   },
   include: {},
   bootstrap: false,
   mocha: {},
   name: "webapp-tests",
-  require: ["puppeteer"],
 };
